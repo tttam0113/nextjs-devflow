fa1d9d75b008c3532380d66a5ae48e92206153c0
tttam0113 <tttam0113@gmail.com>
2024-11-13 14:16:50 +0700

    feat: setup light and dark theme, shadcn, navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 
 import ThemeProvider from '@/context/Theme';
+import Navbar from '@/components/navigation/navbar';
 
 const inter = localFont({
   src: './fonts/InterVF.ttf',
@@ -39,6 +40,7 @@ export default function RootLayout({
           defaultTheme='system'
           enableSystem
           disableTransitionOnChange>
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
